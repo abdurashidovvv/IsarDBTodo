5dd5fad126178866f2a3cc4218f83e2ee76c0076
Saydullo <abdurashidovsaydullo35@gmail.com>
2024-05-27 14:47:47 +0500

    Initialize Isar Database in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_isar/models/note_database.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NoteDatabase.initialize();
   runApp(const MyApp());
 }
 
